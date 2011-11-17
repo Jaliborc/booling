882cb74c4e67754b3fc9d22a480a22ed0ad20552
Jaliborc <contact@jaliborc.com>
2011-11-17 16:03:44 +0000

    Fixed the firefox bug with the "and" and "or" chars

--- Brain/Constants.js
+++ Brain/Constants.js
@@ -3,5 +3,5 @@ Online = true;
 T = '<a class="green">T</a>';
 F = '<a class="red">F</a>';
 NOT = '¬';
-Keys = [['¬', ['no', '~', '!'], [78]], ['⋀', ['and', '^'], [65, 73]], ['⋁', ['or', 'union'], [79, 85]], ['⇒', ['then', '->'], [84]], ['⇔', ['is', 'equal', 'same', '='], [73, 69]]];
+Keys = [['¬', ['no', '~', '!'], [78]], ['∧', ['and', '^'], [65, 73]], ['∨', ['or', 'union'], [79, 85]], ['⇒', ['then', '->'], [84]], ['⇔', ['is', 'equal', 'same', '='], [73, 69]]];
 Placeholders = ['Enter a formula or prepare for humiliation...', "Enter a formula. You don't want to see me bored...", 'Are you going to enter a formula or not? Losing my patience here.', 'Being bullied? "Just act less gay", advise teachers.'];
